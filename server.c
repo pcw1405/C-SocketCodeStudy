f66ed82892eaf795798b260bc8c0d47dca9fb12a
chungwon <pcw14057@gmail.com>
2024-08-19 15:40:29 +0900

    오타수정 및 주석 완성

--- server.c
+++ server.c
@@ -19,7 +19,7 @@ typedef struct ReqLogin
 int recvLoginPacket(SOCKET hSOCKET)
 {
 	char message[BUFSIZE];
-	// 메시지를 초기화한다
+	// 데이터를 저장할 버퍼를 선언합니다.
 	short code=0,size=0;
 	// 패킷의 코드와 크기를 저장할 short형 변수를 각각 0으로 초기화합니다.
 	int iret,result;
@@ -47,7 +47,8 @@ int recvLoginPacket(SOCKET hSOCKET)
 		
 		memset(&message,0x00,2);
 		//memset(&message, 0x00, 2);: message 버퍼의 처음 2바이트를 0으로 초기화합니다.
-		
+		//첫 번째 초기화는 code를 추출한 후, 버퍼의 처음 2바이트를 초기화하여 그 부분이 사용되지 않는다는 것을 명확히 합니다
+
 		recv(hSOCKET,message,2,0);
 		//다시 클라이언트로부터 2바이트의 데이터를 수신하여 message에 저장합니다. 이 데이터는 이후에 패킷의 크기를 나타냅니다.
 
@@ -56,12 +57,31 @@ int recvLoginPacket(SOCKET hSOCKET)
 
 		memset(&message,0x00,2);
 		//버퍼의 처음 2바이트를 다시 0으로 초기화합니다.
+		//두 번째 초기화는 size를 추출한 후, 버퍼를 다시 초기화하여 이전 데이터를 제거하고 새로운 데이터를 받을 준비를 합니다. 
 
-
-	}
+	}	//버퍼는 깨끗한상태로 만든다 
 
 	if(size>0)
 		recv(hSOCKET,message,size,0);
 
+
+	// size가 0보다 크면, 실제 데이터를 수신하여 message 버퍼에 저장합니다.
+
 	printf("데이터 수신 : code=%d,size=%d\n",code,size,0);
+  // 수신된 코드와 데이터 크기를 출력합니다.
+
+	if(code==C2S_REQ_LOGIN)
+	{
+		print("로그인 요청 수신\n");
+		result=trxlogin(hSOCKET,message);
+	// 패킷 코드가 로그인 요청이면 로그인 처리 함수를 호출합니다.
+		return result;
+	}
+	 // 로그인 처리 결과를 반환합니다.
+	else
+	{
+		printf("패킷 코드 오류\n");
+		return -1;
+		 // 패킷 코드가 로그인 요청이 아니면 오류 메시지를 출력하고 -1을 반환합니다.
+	}
 }
